3863e2bc881ede45a3d403799adb2836fe73a618
nicholaslyang <nicholas.yang@vercel.com>
2024-09-25 16:21:09 -0400

    Adding cache parser

--- crates/turborepo-lib/src/config/mod.rs
+++ crates/turborepo-lib/src/config/mod.rs
@@ -1,9 +1,11 @@
+mod cache;
 mod env;
 mod file;
 mod turbo_json;
 
 use std::{collections::HashMap, ffi::OsString, io};
 
+use biome_deserialize_macros::Deserializable;
 use camino::{Utf8Path, Utf8PathBuf};
 use convert_case::{Case, Casing};
 use derive_setters::Setters;
@@ -11,7 +13,7 @@ use env::{EnvVars, OverrideEnvVars};
 use file::{AuthFile, ConfigFile};
 use merge::Merge;
 use miette::{Diagnostic, NamedSource, SourceSpan};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use struct_iterable::Iterable;
 use thiserror::Error;
 use turbo_json::TurboJsonReader;
@@ -47,6 +49,8 @@ pub struct InvalidEnvPrefixError {
 pub enum Error {
     #[error("Authentication error: {0}")]
     Auth(#[from] turborepo_auth::Error),
+    #[error(transparent)]
+    Cache(#[from] cache::Error),
     #[error("Global config path not found")]
     NoGlobalConfigPath,
     #[error("Global auth file path not found")]
@@ -188,6 +192,18 @@ const DEFAULT_API_URL: &str = "https://vercel.com/api";
 const DEFAULT_LOGIN_URL: &str = "https://vercel.com";
 const DEFAULT_TIMEOUT: u64 = 30;
 const DEFAULT_UPLOAD_TIMEOUT: u64 = 60;
+
+#[derive(Serialize, Deserialize, Deserializable, Debug, PartialEq, Eq, Clone, Default)]
+pub struct CacheActions {
+    read: bool,
+    write: bool,
+}
+
+#[derive(Serialize, Deserialize, Deserializable, Debug, PartialEq, Eq, Clone, Default)]
+pub struct CacheConfig {
+    fs: CacheActions,
+    remote: CacheActions,
+}
 
 // We intentionally don't derive Serialize so that different parts
 // of the code that want to display the config can tune how they
